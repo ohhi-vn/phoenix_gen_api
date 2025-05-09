93513c3da28ea28c424951ab4d7ba735821c2fc0
Manh Van Vu <vuvanmanh123@gmail.com>
2025-05-09 20:29:10 +0700

    fix function with no argument

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixGenApi.MixProject do
   def project do
     [
       app: :phoenix_gen_api,
-      version: "0.0.10",
+      version: "0.0.12",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
